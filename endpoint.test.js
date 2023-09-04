15144fd8297748c99fb168ca2dca2a409d7f3e7d
rezamohdev <mohammadireza29@gmail.com>
2023-09-04 16:51:45 -0400

    test

--- endpoint.test.js
+++ endpoint.test.js
@@ -4,3 +4,21 @@ const app = require('./app.js');
 
 
 const request = supertest(app);
+
+
+describe('Testing HTTP Requests', () => {
+    it("Returns the string 'Hello, world!' and the correct status", () => {
+        return request.get('/').then((response) => {
+            expect(response.status).toBe(200);
+            expect(response.text).toBe('Hello, world!');
+        })
+    });
+
+    it("A POST request to the /users address returns a JSON object with user data and the correct status indicating that the user has been created.", () => {
+        return request.post('/users').then((response) => {
+            expect(response.text).toBe('success');
+            expect(response.body.data.isDeveloper).toBe(true);
+            expect(response.body.data.followersOnGithub).toBeGreaterThan(10);
+        })
+    });
+});
