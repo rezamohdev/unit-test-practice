386e713637f1291d8ead9c2f571fea0f7866f479
rezamohdev <mohammadireza29@gmail.com>
2023-09-04 17:37:24 -0400

    httt test

--- task-TestingHttpRequests/__tests__/test.index.js
+++ task-TestingHttpRequests/__tests__/test.index.js
@@ -5,3 +5,23 @@ const request = supertest(app);
 
 // Write tests here
 
+
+describe('Testing HTTP Requests', () => {
+    it("Returns the string 'Hello, world!' and the correct status", () => {
+        return request.get('/').then((response) => {
+            expect(response.status).toBe(200);
+            expect(response.text).toBe('Hello, world!');
+        })
+    });
+
+    it("A POST request to the /users address returns a JSON object with user data and the correct status indicating that the user has been created.", () => {
+        return request.post('/users').then((response) => {
+            expect(response.status).toBe(201);
+            expect(response.headers['content-type']).toMatch('application/json');
+
+            expect(response.body.message).toBe('success');
+            expect(response.body.data.isDeveloper).toBeTruthy();
+            expect(response.body.data.followersOnGithub).toBeGreaterThan(10);
+        })
+    });
+});
